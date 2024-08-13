b1ec6a0c0d6b32a5af9224c3823ece3c2d567dbc
Bruno Trigueiro <bruno.trigueiro@outlook.com>
2024-08-13 20:20:39 -0300

    Alterações no cálculo do desvio padrão e limites de tolerância

--- func/tester.py
+++ func/tester.py
@@ -6,13 +6,15 @@ def benford_law_array():
 def calulate_standard_deviations(day_by_day_variance):
     data_size = len(day_by_day_variance)
     arr_benford = benford_law_array()
-    return [math.sqrt((arr_benford[i] * (1 - arr_benford[i])) / data_size) for i in range(9)]
+    return [math.pow(((arr_benford[i] * (1 - arr_benford[i])) / data_size), 1/2) for i in range(9)]
 
 def calculate_area_of_tolerance(day_by_day_variance):
+    total_observations = len(day_by_day_variance
+    )
     arr_benford = benford_law_array()
     standard_deviations = calulate_standard_deviations(day_by_day_variance)
-    lower_bounds = [arr_benford[i] - 1.96 * standard_deviations[i] for i in range(9)]
-    upper_bounds = [arr_benford[i] + 1.96 * standard_deviations[i] for i in range(9)]
+    lower_bounds = [arr_benford[i] - (1.96 * standard_deviations[i] - 1/(2*total_observations)) for i in range(9)]
+    upper_bounds = [arr_benford[i] + (1.96 * standard_deviations[i] + 1/(2*total_observations)) for i in range(9)]
     return lower_bounds, upper_bounds
 
 def calculate_MAD(frequency):
